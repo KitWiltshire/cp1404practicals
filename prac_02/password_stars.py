fb0be6d0b883b6cd0d1735829b599cae80cd6908
Kit <kit.wiltshire@my.jcu.edu.au>
2022-09-25 17:02:19 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,14 +1,17 @@
 def main():
+    """Asks for password and turns it into asterisks or stars"""
     password = get_password()
     create_stars(password)
 
 
 def get_password():
+    """this is the code that gets an input from the user for the password"""
     password = input("Enter password: ")
     return password
 
 
 def create_stars(password):
+    """converts a string of characters into just asterisks"""
     for i in range(len(password)):
         print("*", end='')
 
