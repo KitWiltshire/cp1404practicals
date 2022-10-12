9ac3962078f6f03d783f3ef4943e34d1ad65c2aa
Kit <kit.wiltshire@my.jcu.edu.au>
2022-10-12 23:00:04 +1000

    weak warnings have been fixed

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,7 +12,8 @@ print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {average}")
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 username = input("Enter username: ")
 if username in usernames:
     print("Access granted")
