3fa46c3d068f13d069489d7b57c50a4abe1b59de
Kit <kit.wiltshire@my.jcu.edu.au>
2022-10-19 21:40:28 +1000

    Make a for loop of all the states and make them neatly aligned

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -12,7 +12,8 @@ print(CODE_TO_NAME)
 state_code = input("Enter short state: ")
 while state_code != "":
     if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
+
