7618ea693f68d93e275894087336360d1783f842
Kit <kit.wiltshire@my.jcu.edu.au>
2022-10-24 21:50:24 +1000

    Start writing the most basic parts of the script e.g. the startup text.

---
+++ assignment1/assignment1.py
@@ -0,0 +1,15 @@
+"""
+Name: Kit Wiltshire
+Date started: 24/10/2022
+GitHub URL:
+"""
+
+
+def main():
+    """..."""
+    print("Movies To Watch 1.0 - by Kit Wiltshire")
+
+
+
+if __name__ == '__main__':
+    main()
