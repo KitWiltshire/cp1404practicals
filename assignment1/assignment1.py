46208d5f8c9b15b6cb42f56ba6c6950bfbc0ad98
Kit <kit.wiltshire@my.jcu.edu.au>
2022-10-24 23:26:04 +1000

    figure out how to get movies.csv to work into this.

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -6,8 +6,12 @@ GitHub URL:
 
 
 def main():
-    """..."""
+    """This function will be in charge of the whole program with other functions inside that do the complex
+    things """
     print("Movies To Watch 1.0 - by Kit Wiltshire")
+    MENU = "D - Display movies \nA - Add new movie \nW - Watch a movie \Q - Quit"
+    print(MENU)
+    menu_input = input(">>> ")
 
 
 
