2d5a394b0a10e80632f4b51d292c0dde0d7d844c
Kit <kit.wiltshire@my.jcu.edu.au>
2022-10-26 17:56:01 +1000

    Make the code for adding a new movie and fix it up so getting a valid year works properly and it will actually add it all to the csv file.

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -8,10 +8,20 @@ GitHub URL:
 def main():
     """This function will be in charge of the whole program with other functions inside that do the complex
     things """
+    CATEGORIES = ["Action", "Comedy", "Documentary", "Drama", "Thriller", "Other"]
     print("Movies To Watch 1.0 - by Kit Wiltshire")
     MENU = "D - Display movies \nA - Add new movie \nW - Watch a movie \nQ - Quit"
     print(MENU)
     menu_input = get_valid_menu_input()
+    movies_backup_file = open("movies_backup.csv", "r")
+    movies_file = open("movies.csv", "w")
+    initial_movies_content = movies_backup_file.read()
+    movies_file.write(initial_movies_content)
+    print(initial_movies_content)
+    if menu_input == "D":
+        print(initial_movies_content)
+    elif menu_input == "A":
+        add_new_movie(CATEGORIES)
 
 
 
@@ -22,6 +32,38 @@ def get_valid_menu_input():
         print("Invalid menu choice")
         menu_input = input(">>> ").upper()
     return menu_input
+
+# name = input("Name: ")
+# out_file = open("minecraft.txt", "w")
+# print(name, file=out_file)
+# print(out_file)
+# out_file.close()
+
+
+def add_new_movie(CATEGORIES):
+    movies = []
+    movie_title = input("Title: ")
+    while movie_title == "":
+        print("Input can not be blank")
+        movie_title = input("Title: ")
+    try:
+        movie_year = int(input("Year: "))
+        while movie_year != int and movie_year < 1:
+            if movie_year == "":
+                print("Input can not be blank")
+            elif movie_year < 1:
+                print("Number must be >= 1")
+            movie_year = int(input("Year: "))
+    except ValueError:
+        print("Invalid input; enter a valid number")
+        movie_year = input("Year: ")
+    # TODO: Fix the code so it will continue to ask for a year if the input is not a number
+    print(f"Categories available: {CATEGORIES}")
+    movie_category = input("Category: ").capitalize()
+    if movie_category not in CATEGORIES:
+        print("Invalid category; using Other")
+        movie_category = "Other"
+    print(f"{movie_title} ({movie_category} from {movie_year}) added to movie list")
 
 
 if __name__ == '__main__':
